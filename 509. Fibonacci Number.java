0672261710f648eab8e0a13380e2fbf28dce0954
varman333 <jayavarman272@gmail.com>
2026-07-27 10:57:13 +0530

    Create 509. Fibonacci Number.java

---
+++ 509. Fibonacci Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int prev2 = 0;
+        int prev1 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
