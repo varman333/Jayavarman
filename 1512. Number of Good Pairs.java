7dbf937f5d2e158652610a0fc79e23a08b72e68e
varman333 <jayavarman272@gmail.com>
2026-07-27 13:12:06 +0530

    Create 1512. Number of Good Pairs.java

---
+++ 1512. Number of Good Pairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int[] count = new int[101];
+        int goodPairs = 0;
+
+        for (int num : nums) {
+            goodPairs += count[num];
+            count[num]++;
+        }
+
+        return goodPairs;
+    }
+}
