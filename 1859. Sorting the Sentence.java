0b3111bf0658e11ae57aa99c6210058f4d184f4c
varman333 <jayavarman272@gmail.com>
2026-07-27 13:08:35 +0530

    Create 1859. Sorting the Sentence.java

---
+++ 1859. Sorting the Sentence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] result = new String[words.length];
+
+        for (String word : words) {
+            int index = word.charAt(word.length() - 1) - '1';
+            result[index] = word.substring(0, word.length() - 1);
+        }
+
+        return String.join(" ", result);
+    }
+}
